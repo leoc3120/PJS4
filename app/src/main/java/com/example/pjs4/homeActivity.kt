791e061792a6b364f5ae553dabe934b1b0b6d05e
Exteal <rh.4@laposte.net>
2022-02-15 17:54:54 +0100

    Java???

--- app/src/main/java/com/example/pjs4/homeActivity.kt
+++ app/src/main/java/com/example/pjs4/homeActivity.kt
@@ -1,14 +1,11 @@
 package com.example.pjs4
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-
-
-class MainActivity : AppCompatActivity() {
-
+class homeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
