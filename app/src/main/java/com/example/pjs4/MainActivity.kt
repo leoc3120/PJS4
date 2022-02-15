0e8788c0bfa2c2cf7d00941137835cb496411754
leoc3120 <58136950+leoc3120@users.noreply.github.com>
2022-02-15 15:47:54 +0100

    ajout des boutons

--- app/src/main/java/com/example/pjs4/MainActivity.kt
+++ app/src/main/java/com/example/pjs4/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.pjs4
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
