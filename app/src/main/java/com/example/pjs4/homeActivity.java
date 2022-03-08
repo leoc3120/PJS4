3a987e6801ccf51badac5c56c7f021b1b0dc523b
leoc3120 <58136950+leoc3120@users.noreply.github.com>
2022-03-08 16:05:22 +0100

    Navigation entre page principale et settings

--- app/src/main/java/com/example/pjs4/homeActivity.java
+++ app/src/main/java/com/example/pjs4/homeActivity.java
@@ -1,13 +1,26 @@
 package com.example.pjs4;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class homeActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        View view  = findViewById(R.id.page_4);
+
+        view.setOnClickListener(v -> {
+            Intent homeActivityIntent = new Intent(this, settingsActivity.class);
+            startActivity(homeActivityIntent);
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/pjs4/settingsActivity.java
@@ -0,0 +1,23 @@
+package com.example.pjs4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class settingsActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+
+        }
+        
+    }
