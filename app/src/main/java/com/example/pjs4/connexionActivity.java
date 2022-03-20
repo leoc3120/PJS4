c796faf2ee54b1267f907b6ac4df442698908cb4
Anixxi <79166272+Anixxi@users.noreply.github.com>
2022-03-20 20:26:01 +0100

    Connexion Firebase

--- app/src/main/java/com/example/pjs4/connexionActivity.java
+++ app/src/main/java/com/example/pjs4/connexionActivity.java
@@ -1,23 +1,98 @@
 package com.example.pjs4;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class connexionActivity extends AppCompatActivity {
+
+    EditText email, password;
+    Button btnLogin;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog progressDialog;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_connexion);
 
+        setContentView(R.layout.activity_connexion);
         Button connexion = findViewById(R.id.btnConnexion);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        btnLogin = findViewById(R.id.btnConnexion);
+        progressDialog = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
 
         connexion.setOnClickListener(v -> {
             Intent homeActivityIntent = new Intent(connexionActivity.this, homeActivity.class);
             startActivity(homeActivityIntent);
         });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PerforLogin();
+            }
+        });
+
+    }
+
+    private void PerforLogin() {
+
+        String mail = email.getText().toString();
+        String mdp = password.getText().toString();
+
+        if (!mail.matches(emailPattern)) {
+
+            email.setError("Entrer un mail correct");
+        } else if (mdp.isEmpty() || password.length() < 6) {
+            password.setError("Entrer un mot de passe correct");
+        } else {
+            progressDialog.setMessage("Veuillez patientez pendant la connexion");
+            progressDialog.setTitle("Connexion");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.signInWithEmailAndPassword(mail, mdp).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if (task.isSuccessful()) {
+                        progressDialog.dismiss();
+                        sendToNextActivity();
+                        Toast.makeText(connexionActivity.this, "Connexion rÃ©ussie", Toast.LENGTH_SHORT).show();
+                    } else {
+                        progressDialog.dismiss();
+                        Toast.makeText(connexionActivity.this, "" + task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
+        }
+    }
+
+    private void sendToNextActivity() {
+        Intent intent = new Intent(connexionActivity.this, homeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/pjs4/settingsActivity.java
+++ app/src/main/java/com/example/pjs4/settingsActivity.java
@@ -2,7 +2,11 @@ package com.example.pjs4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationBarView;
 
 public class settingsActivity extends AppCompatActivity {
 
@@ -10,5 +14,38 @@ public class settingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        NavigationBarView nav = findViewById(R.id.bottom_navigation);
+        nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                int id = item.getItemId();
+                switch(id){
+                    case R.id.btn1:
+                        Intent startActivityIntent = new Intent(settingsActivity.this, startActivity.class);
+                        startActivity(startActivityIntent);
+                        break;
+
+                    case R.id.btn2:
+                        Intent searchActivityIntent = new Intent(settingsActivity.this, searchActivity.class);
+                        startActivity(searchActivityIntent);
+                        break;
+
+                    case R.id.btn3:
+                        Intent profileActivityIntent = new Intent(settingsActivity.this, profileActivity.class);
+                        startActivity(profileActivityIntent);
+                        break;
+
+                    case R.id.btn4:
+                        Intent settingsActivityIntent = new Intent(settingsActivity.this, settingsActivity.class);
+                        startActivity(settingsActivityIntent);
+                        break;
+                    default :
+                        break;
+                }
+                return true;
+            }
+        });
     }
+
 }
