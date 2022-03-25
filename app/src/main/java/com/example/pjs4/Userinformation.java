887ddeb486354f3ef71ddb349894f7c5bacbeb78
SanaHar3 <92450870+SanaHar3@users.noreply.github.com>
2022-03-26 00:18:09 +0100

    Update Userinformation.java

--- app/src/main/java/com/example/pjs4/Userinformation.java
+++ app/src/main/java/com/example/pjs4/Userinformation.java
@@ -3,40 +3,38 @@ package com.example.pjs4;
 public class Userinformation {
 
     public String Name;
+    public String Height;
+    public String Weight;
     public String Sex;
     public String Birth;
     public String Country;
-    public String Weight;
-    public String Height;
-
 
     public Userinformation(){
     }
 
-    public Userinformation(String Name,String Sex, String Birth, String Country, String Weight, String Height){
+    public Userinformation(String Name,String Height, String Weight, String Sex, String Birth, String Country){
         this.Name = Name;
+        this.Height = Height;
+        this.Weight = Weight;
         this.Sex = Sex;
         this.Birth = Birth;
         this.Country = Country;
-        this.Weight = Weight;
-        this.Height = Height;
+
     }
     public String getUserName() {
         return Name;
     }
-    public String getUserBirth() {
-        return Birth;
-    }
-    public String getUserSex() {
-        return Sex;
-    }
-    public String getUserCountry() {
-        return Country;
+    public String getUserHeight() {
+        return Height;
     }
     public String getUserWeight() {
         return Weight;
     }
-    public String getUserHeight() {
-        return Height;
+    public String getUserSex() { return Sex; }
+    public String getUserBirth() { return Birth; }
+    public String getUserCountry() {
+        return Country;
     }
+
+
 }
