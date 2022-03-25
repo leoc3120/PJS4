ef7886130545aa7f4b6a3de4e22ef04aab630f2a
SanaHar3 <92450870+SanaHar3@users.noreply.github.com>
2022-03-25 22:35:14 +0100

    EditProfile changements

--- app/src/main/java/com/example/pjs4/Userinformation.java
+++ app/src/main/java/com/example/pjs4/Userinformation.java
@@ -2,25 +2,41 @@ package com.example.pjs4;
 
 public class Userinformation {
 
-    public String name;
-    public String surname;
-    public String phoneno;
+    public String Name;
+    public String Sex;
+    public String Birth;
+    public String Country;
+    public String Weight;
+    public String Height;
+
 
     public Userinformation(){
     }
 
-    public Userinformation(String name,String surname, String phoneno){
-        this.name = name;
-        this.surname = surname;
-        this.phoneno = phoneno;
+    public Userinformation(String Name,String Sex, String Birth, String Country, String Weight, String Height){
+        this.Name = Name;
+        this.Sex = Sex;
+        this.Birth = Birth;
+        this.Country = Country;
+        this.Weight = Weight;
+        this.Height = Height;
     }
     public String getUserName() {
-        return name;
+        return Name;
     }
-    public String getUserSurname() {
-        return surname;
+    public String getUserBirth() {
+        return Birth;
     }
-    public String getUserPhoneno() {
-        return phoneno;
+    public String getUserSex() {
+        return Sex;
+    }
+    public String getUserCountry() {
+        return Country;
+    }
+    public String getUserWeight() {
+        return Weight;
+    }
+    public String getUserHeight() {
+        return Height;
     }
 }
