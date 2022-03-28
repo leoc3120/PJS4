853951acad515f092d10437707c3d58bb6d76d12
SanaHar3 <92450870+SanaHar3@users.noreply.github.com>
2022-03-28 14:17:22 +0200

    small changes profil

--- app/src/main/java/com/example/pjs4/profileActivity.java
+++ app/src/main/java/com/example/pjs4/profileActivity.java
@@ -1,23 +1,19 @@
 package com.example.pjs4;
-
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -79,40 +75,103 @@ public class profileActivity  extends AppCompatActivity {
             finish();
             startActivity(new Intent(getApplicationContext(), connexionActivity.class));
         }
+        final FirebaseUser user = firebaseAuth.getCurrentUser();
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Userinformation userProfile = dataSnapshot.getValue(Userinformation.class);
+                profileNameTextView.setText(userProfile.getUserName());
+                profileHeightTextView.setText(userProfile.getUserHeight());
+                profileWeightTextView.setText(userProfile.getUserWeight());
+                profileSexTextView.setText(userProfile.getUserSex());
+                profileBirthTextView.setText(userProfile.getUserBirth());
+                profileCountryTextView.setText(userProfile.getUserCountry());
+                textViewemailname = (TextView) findViewById(R.id.textViewEmailAdress);
+                textViewemailname.setText(user.getEmail());
+            }
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView)
-                findViewById(R.id.bottom_navigation);
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(profileActivity.this, databaseError.getCode(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(MenuItem item) {
-                        switch (item.getItemId()) {
-                            case R.id.btn1:
-                                Intent startActivityIntent = new Intent(profileActivity.this, homeActivity.class);
-                                startActivity(startActivityIntent);
-                                break;
+    private void navigateBack(View v) {
+        Intent in = new Intent(this, settingsActivity.class);
+        startActivity(in);
+    }
 
-                            case R.id.btn2:
-                                Intent searchActivityIntent = new Intent(profileActivity.this, searchActivity.class);
-                                startActivity(searchActivityIntent);
-                                break;
+    public void buttonClickedEditName(View view) {
 
-                            case R.id.btn3:
-                                Intent profileActivityIntent = new Intent(profileActivity.this, profileActivity.class);
-                                startActivity(profileActivityIntent);
-                                break;
+        LayoutInflater inflater = getLayoutInflater();
+        View alertLayout = inflater.inflate(R.layout.layout_custom_dialog_edit_name, null);
+        final EditText etUsername = alertLayout.findViewById(R.id.et_username);
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Name Edit");
+        // this is set the view from XML inside AlertDialog
+        alert.setView(alertLayout);
+        // disallow cancel of AlertDialog on click of back button and outside touch
+        alert.setCancelable(false);
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String Name = etUsername.getText().toString();
+                String Height = profileHeightTextView.getText().toString();
+                String Weight = profileWeightTextView.getText().toString();
+                String Sex = profileSexTextView.getText().toString();
+                String Birth = profileBirthTextView.getText().toString();
+                String Country = profileCountryTextView.getText().toString();
+                Userinformation userinformation = new Userinformation(Name, Height, Weight, Sex, Birth, Country);
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                databaseReference.child(user.getUid()).setValue(userinformation);
+                databaseReference.child(user.getUid()).setValue(userinformation);
+                etUsername.onEditorAction(EditorInfo.IME_ACTION_DONE);
+            }
+        });
+        AlertDialog dialog = alert.create();
+        dialog.show();
+    }
 
-                            case R.id.btn4:
-                                Intent settingsActivityIntent = new Intent(profileActivity.this, settingsActivity.class);
-                                startActivity(settingsActivityIntent);
-                                break;
-                            default :
-                                break;
-                        }
-                        return true;
-                    }
-                });
+    public void buttonClickedEditHeight(View view) {
+        LayoutInflater inflater = getLayoutInflater();
+        View alertLayout = inflater.inflate(R.layout.layout_custom_dialog_edit_height, null);
+        final EditText etUserHeight = alertLayout.findViewById(R.id.et_userHeight);
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Height Edit");
+        // this is set the view from XML inside AlertDialog
+        alert.setView(alertLayout);
+        // disallow cancel of AlertDialog on click of back button and outside touch
+        alert.setCancelable(false);
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                String Name = profileNameTextView.getText().toString();
+                String Height = etUserHeight.getText().toString();
+                String Weight = profileWeightTextView.getText().toString();
+                String Sex = profileSexTextView.getText().toString();
+                String Birth = profileBirthTextView.getText().toString();
+                String Country = profileCountryTextView.getText().toString();
+                Userinformation userinformation = new Userinformation(Name, Height, Weight, Sex, Birth, Country);
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                databaseReference.child(user.getUid()).setValue(userinformation);
+                databaseReference.child(user.getUid()).setValue(userinformation);
+                etUserHeight.onEditorAction(EditorInfo.IME_ACTION_DONE);
+            }
+        });
+        AlertDialog dialog = alert.create();
+        dialog.show();
     }
 
     public void buttonClickedEditWeight(View view) {
@@ -258,11 +317,6 @@ public class profileActivity  extends AppCompatActivity {
     public void navigateLogOut(View v) {
         FirebaseAuth.getInstance().signOut();
         Intent inent = new Intent(this, startActivity.class);
-        startActivity(inent);
-    }
-
-    public void navigateBack(View v){
-        Intent inent = new Intent(this, settingsActivity.class);
         startActivity(inent);
     }
 
